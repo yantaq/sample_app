579c1e7c491212983a7d962899d6ac782c89e63d
asan <yantaq@att.net>
2011-10-21 23:35:39 -0700

    Finished layout and routes with exercise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,9 @@
 module ApplicationHelper
 
   #Return a title based on a per-page basis
+  def logo
+    logo =  image_tag("logo.png", :alt => "Sample App", :class => "round") 
+  end
   def title
       base_title = "Ruby on Rails Tutorials Sample App"
     if @title.nil?
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -25,4 +25,22 @@ describe "LayoutLinks" do
     get '/signup'
     response.should have_selector('title', :content => "Sign up")
   end
+
+  it "It should have the right links on the layout" do
+    visit root_path
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+    
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+
+    visit root_path
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+
+    visit root_path
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+  end
 end
